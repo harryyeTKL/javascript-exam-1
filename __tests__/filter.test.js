84247fb3fca4c926b02192008dac5610bf7a0a29
Harry Ye <harry.ye@cargosmart.com>
2023-11-09 15:07:59 +0800

    filter and find tests

--- __tests__/filter.test.js
+++ __tests__/filter.test.js
@@ -1,5 +1,37 @@
 import { filterEvenNumbers, filterLengthWith4, filterStartWithA } from '../src/filter';
 
-describe('array filter test', () => {
+describe('filterEvenNumbers test: ', () => {
     // Please add test cases here
+    // Given
+    test('filter should retain even numbers', ()=>{
+        const arr = [1,3,4,5,10,-100]
+
+        const result = filterEvenNumbers(arr)
+    
+        expect(result).toEqual([4,10,-100])
+    });
 });
+
+
+describe('filterLengthWith4 test', () => {
+    // Please add test cases here
+    test('filter should retain all string with lenth 4', ()=>{
+        const arr = ['Hello','World','True', 'But','Home']
+
+        const result = filterLengthWith4(arr)
+    
+        expect(result).toEqual(['True','Home'])
+    });
+});
+
+describe('filterStartWithA test', () => {
+    // Please add test cases here
+    test('filter should retain all string with \'a\' as first character', ()=>{
+        const arr = ['Hello','atom','World','True', 'But','home','apple']
+
+        const result = filterStartWithA(arr)
+    
+        expect(result).toEqual(['atom','apple'])
+    });
+});
+
--- __tests__/find.test.js
+++ __tests__/find.test.js
@@ -1,5 +1,43 @@
 import { firstGrownUp, firstOrange, firstLengthOver5Name } from "../src/find";
 
-describe("array find test", () => {
+describe("Find the first grown up ", () => {
   // Please add test cases here
+  it ('should return the first number that is larger than or equal to 18', ()=>{
+    const numbers = [1,3,4,90,12];
+    expect(firstGrownUp(numbers)).toEqual(90);
+  });
+
+  it('should return undefined if there is no grown up in the array', () => {
+    const numbers = [1,3,4,12];
+    expect(firstGrownUp(numbers)).toEqual(undefined);
+  });
+
+});
+
+
+describe("Find the first orange", () => {
+  // Please add test cases here
+  it('should return the first "orange" from the array', () => {
+    const fruits = ['apple', 'banana', 'orange', 'cherry', 'grape'];
+    expect(firstOrange(fruits)).toEqual('orange');
+  });
+
+  it('should return undefined if there is no "orange" in the array', () => {
+    const fruits = ['apple', 'banana', 'cherry', 'grape'];
+    expect(firstOrange(fruits)).toEqual(undefined);
+  });
+});
+
+
+describe("Find the first name that its length is longer that 5", () => {
+  // Please add test cases here
+  it('should return the first "" from the array', () => {
+    const names = ['John', 'Emily', 'Sarah', 'Michael', 'Sophia', 'Jacob', 'Olivia', 'Noah', 'Emma', 'Liam'];
+    expect(firstLengthOver5Name(names)).toEqual('Michael');
+  });
+
+  it('should return undefined if there is no "orange" in the array', () => {
+    const names = ['John','Noah', 'Emma', 'Liam'];
+    expect(firstLengthOver5Name(names)).toEqual(undefined);
+  });
 });
