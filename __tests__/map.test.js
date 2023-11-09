97d085358664c5a9de40e318f6fa8da5b7ae5c7e
Harry Ye <harry.ye@cargosmart.com>
2023-11-09 15:11:41 +0800

    map tests

--- __tests__/map.test.js
+++ __tests__/map.test.js
@@ -1,5 +1,19 @@
 import { addSerialNumber, halfNumbers, spliceNames } from "../src/map";
 
-describe("array map test", () => {
-  // Please add test cases here
+describe('halfNumbers function', () => {
+  it('should return an array with numbers halved', () => {
+    expect(halfNumbers([4, 8, 6])).toEqual([2, 4, 3]);
+  });
+});
+
+describe('spliceNames function', () => {
+  it('should return an array with names prepended by "Hello "', () => {
+    expect(spliceNames(['John', 'Jane'])).toEqual(['Hello John', 'Hello Jane']);
+  });
+});
+
+describe('addSerialNumber function', () => {
+  it('should return an array with each fruit prepended by its index number', () => {
+    expect(addSerialNumber(['Apple', 'Banana'])).toEqual(['1. Apple', '2. Banana']);
+  });
 });
